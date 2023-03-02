4217c913669d40d61740252c2cf03fdd1b80b211
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-03-03 00:19:06 +0100

    loaders

--- remix-blog/app/routes/posts/index.tsx
+++ remix-blog/app/routes/posts/index.tsx
@@ -1,7 +1,33 @@
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const data = {
+    posts: [
+      { id: 1, title: "Post 1", body: "This is a test post." },
+      { id: 2, title: "Post 2", body: "This is a test post." },
+      { id: 3, title: "Post 3", body: "This is a test post." },
+      { id: 4, title: "Post 4", body: "This is a test post." },
+    ],
+  };
+
+  return data;
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>This is the posts routes that contains all posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`${post.id}`}>
+              <h3>{post.title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {posts.length === 0 && <h3>No posts found</h3>}
     </div>
   );
 }
