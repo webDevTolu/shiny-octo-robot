b75ddf92dce5b57b50f0aff9f658df19c96807fb
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-03-02 01:37:40 +0100

    my first changes

--- remix-blog/app/root.tsx
+++ remix-blog/app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "New Remix Blog App",
   viewport: "width=device-width,initial-scale=1",
 });
 
---
+++ remix-blog/app/routes/posts/index.tsx
@@ -0,0 +1,3 @@
+export default function PostIndex() {
+  return <div>PostIndex</div>;
+}
