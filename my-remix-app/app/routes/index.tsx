aed47c34973b01990e69a30d3a7656f6fa17eddd
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-03-03 11:17:42 +0100

    configured tailwind + flowbite

--- my-remix-app/app/routes/index.tsx
+++ my-remix-app/app/routes/index.tsx
@@ -1,29 +1,16 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="https://flowbite.com/getting-started/remix/"
             rel="noreferrer"
+            className="text-lg text-blue-600 hover:underline font-bold"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Flowbite + Remix + Tailwind CSS Tutorial
           </a>
         </li>
       </ul>
